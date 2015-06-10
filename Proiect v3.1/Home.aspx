e5f80ed63689fe6188c6d3c7126302fa177ca0ad
Gavrila Timea <gavrila.timea@gmail.com>
2015-06-11 02:23:20 +0300

    Licenta

--- Proiect v3.1/Home.aspx
+++ Proiect v3.1/Home.aspx
@@ -5,12 +5,33 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
 
 <div class="tagCloud-wrapper">
+    <h2>Nu stii ce sa mai cauti? Te ajutam noi!</h2>
     <div id="tagCloud">
 
 
 
     </div>
 </div>
+
+<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ASPNETDB %>">
+    </asp:SqlDataSource>
+
+<div class="home-recommendation-wrapper">
+    <h3>Topul celor mai bine cotate carti</h3>
+    <asp:Repeater ID="Repeater3" runat="server"  DataSourceID="SqlDataSource1">
+        <ItemTemplate>
+            <div class="recommendation book_box">
+                <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl='<%# "~/IndividualBookPage.aspx?q=" + Eval("CartiId") %>'>
+                    <asp:Image ID="Image1" runat="server" ImageUrl='<%# "~/pozeCoperti/" + Eval("Poza_Coperta") %>' />
+                    <asp:Label ID="Label1" runat="server" Text='<%# Bind("CartiTitlu") %>' CssClass="title"></asp:Label>
+                    <asp:Label ID="Label3" runat="server" Text='<%# Eval("NumeAutor") %>' CssClass="name"></asp:Label>
+                </asp:HyperLink>
+            </div>
+        </ItemTemplate>
+     </asp:Repeater>
+</div>
+
+
 <asp:TextBox ID="TBObject" runat="server" CssClass="object"></asp:TextBox>
 <script type="text/javascript">
     $(document).ready(function () {
--- Proiect v3.1/Home.aspx.cs
+++ Proiect v3.1/Home.aspx.cs
@@ -39,6 +39,44 @@ public partial class Home : System.Web.UI.Page
                 reader2.Close();
                 con.Close();
                 TBObject.Text = obiect;
+
+                bool check = (System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+                if (check)
+                {
+                    string userId = System.Web.Security.Membership.GetUser().ProviderUserKey.ToString();
+
+                    con = ConnectionFactory.getNewSqlConnection();
+                    con.Open();
+                    sql = "SELECT Genuri.Gen, COUNT(*) AS NumarGenuri FROM Carti INNER JOIN Genuri ON Carti.Id_Gen = Genuri.Id  INNER JOIN CartiFavorite ON Carti.Id = CartiFavorite.Id_Carte WHERE CartiFavorite.Id_User = @user GROUP BY Gen";
+                    com = new SqlCommand(sql, con);
+                    com.Parameters.AddWithValue("user", userId);
+                    reader = com.ExecuteReader();
+                    int maxim = 0;
+                    string numeCategorie = "";
+                    while (reader.Read())
+                    {
+                        if (maxim < Convert.ToInt32(reader["NumarGenuri"].ToString()))
+                        {
+                            maxim = Convert.ToInt32(reader["NumarGenuri"].ToString());
+                            numeCategorie = reader["Gen"].ToString();
+                        }
+                    }
+                    reader.Close();
+                    con.Close();
+
+                    SqlDataSource1.SelectCommand = "SELECT TOP 6 Carti.Id AS CartiId, Carti.Titlu AS CartiTitlu, Carti.Poza_Coperta, Carti.Text_Descriere, Genuri.Gen, Autori.Prenume + ' ' + Autori.Nume AS NumeAutor, ROUND(AVG(ISNULL(NoteDateCartilor.Nota, 0)), 0) AS MedieNote FROM Carti INNER JOIN Genuri ON Carti.Id_Gen = Genuri.Id INNER JOIN Autori ON Carti.Id_Autor = Autori.Id LEFT JOIN NoteDateCartilor ON Carti.Id = NoteDateCartilor.Id_Carte WHERE Genuri.Gen = @gen AND NOT EXISTS (SELECT CartiFavorite.Id_Carte FROM CartiFavorite Where CartiFavorite.Id_Carte = Carti.Id AND CartiFavorite.Id_User = @user) GROUP BY Carti.Id, Carti.Titlu, Carti.Poza_Coperta, Carti.Text_Descriere, Genuri.Gen, Autori.Prenume + ' ' + Autori.Nume ORDER BY MedieNote DESC";
+                    SqlDataSource1.SelectParameters.Clear();
+                    SqlDataSource1.SelectParameters.Add("gen", numeCategorie);
+                    SqlDataSource1.SelectParameters.Add("user", userId);
+                    SqlDataSource1.DataBind();
+                    
+                }
+                else
+                {
+                    SqlDataSource1.SelectCommand = "SELECT TOP 6 Carti.Id AS CartiId, Carti.Titlu AS CartiTitlu, Carti.Poza_Coperta, Carti.Text_Descriere, Genuri.Gen, Autori.Prenume + ' ' + Autori.Nume AS NumeAutor, ROUND(AVG(ISNULL(NoteDateCartilor.Nota, 0)), 0) AS MedieNote FROM Carti INNER JOIN Genuri ON Carti.Id_Gen = Genuri.Id INNER JOIN Autori ON Carti.Id_Autor = Autori.Id LEFT JOIN NoteDateCartilor ON Carti.Id = NoteDateCartilor.Id_Carte GROUP BY Carti.Id, Carti.Titlu, Carti.Poza_Coperta, Carti.Text_Descriere, Genuri.Gen, Autori.Prenume + ' ' + Autori.Nume ORDER BY MedieNote DESC";
+                    SqlDataSource1.SelectParameters.Clear();
+                    SqlDataSource1.DataBind();
+                }
                 
             }
             catch (Exception err)
